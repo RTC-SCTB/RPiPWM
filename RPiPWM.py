bbf90970b69ba3d2910b28e91a50017a5e43ceea
Viktor Vorobev <vikeyer@mail.ru>
2018-03-01 14:14:37 +0300

    Добавлена работа с дисплеем, но работает не корректно (точки по всему дисплею, плывут столбцы)

--- RPiPWM.py
+++ RPiPWM.py
@@ -35,16 +35,16 @@ class Battery(threading.Thread):
             self._filteredVoltage = self._filteredVoltage * (1 - self._K) + self.GetVoltageInstant() * self._K
             time.sleep(0.05)
 
-    def _Read(self):    # чтение показаний АЦП
+    def _ReadRaw(self):    # чтение показаний АЦП
         reading = self._i2c.read_i2c_block_data(self._addr, 0x00, 2)
         return (reading[0] << 8) + reading[1]
 
-    def _GetRefVoltage(self):   # преобразование к напряжению, относительно опорного (после предделителя)
-        voltage = (self._Read() / 4095) * self._vRef  # 4095 - число разрядов АЦП
+    def _ReadConverted(self):   # преобразование к напряжению, относительно опорного (после предделителя)
+        voltage = (self._ReadRaw() / 4095) * self._vRef  # 4095 - число разрядов АЦП
         return voltage
 
     def GetVoltageInstant(self):  # возвращает моментальное значение напряжения аккумулятора с АЦП (до предделителя)
-        battery = self._GetRefVoltage() * self._gain
+        battery = self._ReadConverted() * self._gain
         return round(battery, 2)
 
     def Stop(self):     # останавливает треды
@@ -56,41 +56,12 @@ class Battery(threading.Thread):
     def Calibrate(self, exactVoltage):  # подгоняет коэффциент делителя напряжения
         value = 0
         for i in range(100):
-            value += self._GetRefVoltage()
+            value += self._ReadConverted()
             time.sleep(0.01)
         value /= 100
         self._gain = exactVoltage/value
         print("Calibrated value: %d" % self._gain)
     # TODO: возможно сделать калибровку более точной (но вроде как без нее все работает и так)
-
-
-# Регистры для работы с PCA9685
-_PCA9685_ADDRESS    = 0x40
-_MODE1              = 0x00
-_MODE2              = 0x01
-_SUBADR1            = 0x02
-_SUBADR2            = 0x03
-_SUBADR3            = 0x04
-_PRESCALE           = 0xFE
-_LED0_ON_L          = 0x06
-_LED0_ON_H          = 0x07
-_LED0_OFF_L         = 0x08
-_LED0_OFF_H         = 0x09
-_ALL_LED_ON_L       = 0xFA
-_ALL_LED_ON_H       = 0xFB
-_ALL_LED_OFF_L      = 0xFC
-_ALL_LED_OFF_H      = 0xFD
-
-# Биты для работы с PCA9685:
-_RESTART            = 0x80   # при чтении возвращает свое состояние, при записи - разрешает или запрещает перезагрузку
-_SLEEP              = 0x10   # режим энергосбережения (выключен внутренний осциллятор)
-_ALLCALL            = 0x01   # PCA9685 будет отвечать на запрос всех устройств на шине
-_INVRT              = 0x10   # инверсный или неинверсный выход сигнала на микросхеме
-_OUTDRV             = 0x04   # способ подключения светодиодов (см. даташит, нам это вроде не надо)
-
-# при частоте ШИМ 50 Гц (20 мс) получаем
-_min = 205  # 1 мс (~ 4096/20)
-_max = 410  # 2 мс (~ 4096*2/20)
 
 ###
 '''
@@ -105,6 +76,34 @@ forwardMotor - для подключения драйвера моторов, у
 '''
 ###
 
+# Регистры для работы с PCA9685
+_PCA9685_ADDRESS = 0x40
+_MODE1 = 0x00
+_MODE2 = 0x01
+_SUBADR1 = 0x02
+_SUBADR2 = 0x03
+_SUBADR3 = 0x04
+_PRESCALE = 0xFE
+_LED0_ON_L = 0x06
+_LED0_ON_H = 0x07
+_LED0_OFF_L = 0x08
+_LED0_OFF_H = 0x09
+_ALL_LED_ON_L = 0xFA
+_ALL_LED_ON_H = 0xFB
+_ALL_LED_OFF_L = 0xFC
+_ALL_LED_OFF_H = 0xFD
+
+# Биты для работы с PCA9685:
+_RESTART = 0x80     # при чтении возвращает свое состояние, при записи - разрешает или запрещает перезагрузку
+_SLEEP = 0x10       # режим энергосбережения (выключен внутренний осциллятор)
+_ALLCALL = 0x01     # PCA9685 будет отвечать на запрос всех устройств на шине
+_INVRT = 0x10       # инверсный или неинверсный выход сигнала на микросхеме
+_OUTDRV = 0x04      # способ подключения светодиодов (см. даташит, нам это вроде не надо)
+
+# при частоте ШИМ 50 Гц (20 мс) получаем
+_min = 205  # 1 мс (~ 4096/20)
+_max = 410  # 2 мс (~ 4096*2/20)
+
 
 class PwmMode(IntEnum):     # список режимов работы
     servo90 = 90            # серва 90 градусов
@@ -118,18 +117,18 @@ class Pwm:
     def __init__(self):
         self._i2c = I2C.SMBus(1)
         # инициализируем микросхему
-        self._WriteByte(_MODE2, _OUTDRV)
-        self._WriteByte(_MODE1, _ALLCALL)
+        self._Write8(_MODE2, _OUTDRV)
+        self._Write8(_MODE1, _ALLCALL)
         time.sleep(0.005)
         mode1 = self._ReadU8(_MODE1)    # читаем установленный режим
         mode1 = mode1 & ~_SLEEP     # будим
-        self._WriteByte(_MODE1, mode1)
+        self._Write8(_MODE1, mode1)
         time.sleep(0.005)
         self._SetPwmFreq(50)    # устанавливаем частоту сигнала 50 Гц
         # словарь, содержащий номера каналов и выставленный режим
         self._channel = {}
 
-    def _WriteByte(self, register, value):  # запись 8битного значения в заданный регистр
+    def _Write8(self, register, value):  # запись 8битного значения в заданный регистр
         value = value & 0xFF
         self._i2c.write_byte_data(_PCA9685_ADDRESS, register, value)
 
@@ -145,17 +144,17 @@ class Pwm:
         prescale = int(math.floor(prescaleval + 0.5))
         oldmode = self._ReadU8(_MODE1)  # смотрим какой режим был у микросхемы
         newmode = (oldmode & 0x7F) | 0x10   # отключаем внутреннее тактирование, чтобы внести изменения
-        self._WriteByte(_MODE1, newmode)
-        self._WriteByte(_PRESCALE, prescale)    # изменяем частоту
-        self._WriteByte(_MODE1, oldmode)    # включаем тактирование обратно
+        self._Write8(_MODE1, newmode)
+        self._Write8(_PRESCALE, prescale)    # изменяем частоту
+        self._Write8(_MODE1, oldmode)    # включаем тактирование обратно
         time.sleep(0.005)   # ждем пока оно включится
-        self._WriteByte(_MODE1, oldmode | 0x08)     # разрешаем микросхеме отвечать на subaddress 1
+        self._Write8(_MODE1, oldmode | 0x08)     # разрешаем микросхеме отвечать на subaddress 1
 
     def _SetPwm(self, channel, value):  # установка значения для канала
-        self._WriteByte(_LED0_ON_L + 4 * channel, 0 & 0xFF)  # момент включения в цикле
-        self._WriteByte(_LED0_ON_H + 4 * channel, 0 >> 8)
-        self._WriteByte(_LED0_OFF_L + 4 * channel, value & 0xFF)  # момент выключения в цикле
-        self._WriteByte(_LED0_OFF_H + 4 * channel, value >> 8)
+        self._Write8(_LED0_ON_L + 4 * channel, 0 & 0xFF)  # момент включения в цикле
+        self._Write8(_LED0_ON_H + 4 * channel, 0 >> 8)
+        self._Write8(_LED0_OFF_L + 4 * channel, value & 0xFF)  # момент выключения в цикле
+        self._Write8(_LED0_OFF_H + 4 * channel, value >> 8)
 
     def Reset(self):    # программный сброс микросхемы
         val = 0x06 & 0xFF
@@ -187,6 +186,256 @@ class Pwm:
             value += 205    # сдвигаем 0-205 -> 205-410
         self._SetPwm(channel, int(value))  # устанавливаем значение
     # TODO: убедиться с разными сервами, что все работает. Возможно добавить режим торможения для мотора с реверсом.
+
+###
+'''
+Классы для работы с дисплеем.
+'''
+###
+# Регистры для работы с SSD1306
+_SSD1306_I2C_ADDRESS = 0x3C    # 011110+SA0+RW - 0x3C or 0x3D
+_SSD1306_SETCONTRAST = 0x81
+_SSD1306_DISPLAYALLON_RESUME = 0xA4
+_SSD1306_DISPLAYALLON = 0xA5
+_SSD1306_NORMALDISPLAY = 0xA6
+_SSD1306_INVERTDISPLAY = 0xA7
+_SSD1306_DISPLAYOFF = 0xAE
+_SSD1306_DISPLAYON = 0xAF
+_SSD1306_SETDISPLAYOFFSET = 0xD3
+_SSD1306_SETCOMPINS = 0xDA
+_SSD1306_SETVCOMDETECT = 0xDB
+_SSD1306_SETDISPLAYCLOCKDIV = 0xD5
+_SSD1306_SETPRECHARGE = 0xD9
+_SSD1306_SETMULTIPLEX = 0xA8
+_SSD1306_SETLOWCOLUMN = 0x00
+_SSD1306_SETHIGHCOLUMN = 0x10
+_SSD1306_SETSTARTLINE = 0x40
+_SSD1306_MEMORYMODE = 0x20
+_SSD1306_COLUMNADDR = 0x21
+_SSD1306_PAGEADDR = 0x22
+_SSD1306_COMSCANINC = 0xC0
+_SSD1306_COMSCANDEC = 0xC8
+_SSD1306_SEGREMAP = 0xA0
+_SSD1306_CHARGEPUMP = 0x8D
+_SSD1306_EXTERNALVCC = 0x1
+_SSD1306_SWITCHCAPVCC = 0x2
+# Константы для работы с прокруткой дисплея
+_SSD1306_ACTIVATE_SCROLL = 0x2F
+_SSD1306_DEACTIVATE_SCROLL = 0x2E
+_SSD1306_SET_VERTICAL_SCROLL_AREA = 0xA3
+_SSD1306_RIGHT_HORIZONTAL_SCROLL = 0x26
+_SSD1306_LEFT_HORIZONTAL_SCROLL = 0x27
+_SSD1306_VERTICAL_AND_RIGHT_HORIZONTAL_SCROLL = 0x29
+_SSD1306_VERTICAL_AND_LEFT_HORIZONTAL_SCROLL = 0x2A
+
+# TODO: тщательно все перепроверить. Точки по всему дисплею, плывут столбцы текста.
+class SSD1306Base(object):  # Базовый класс для работы с OLED дисплеями на базе SSD1306
+    def __init__(self, width, height, i2cBus = None, i2cAddr = _SSD1306_I2C_ADDRESS):
+        self.width = width  # ширина и высота дисплея
+        self.height = height
+        self._pages = height//8     # строки дисплея
+        self._buffer = [0]*(width*self._pages)  # буффер изображения (из нулей)
+        if i2cBus is None:
+            self._i2c = I2C.SMBus(1)
+        else:
+            self._i2c = i2cBus
+
+    def _Initialize(self):
+        raise NotImplementedError
+
+    def Command(self, c):  # Отправка байта команды дисплею
+        control = 0x00
+        self._Write8(control, c)
+
+    def Data(self, c):  # Отправка байта данных дисплею
+        control = 0x40
+        self._Write8(control, c)
+
+    def Begin(self, vccstate=_SSD1306_SWITCHCAPVCC):    # инициализация дисплея
+        self._vccstate = vccstate
+        self._Initialize()
+        self.Command(_SSD1306_DISPLAYON)
+
+    def Display(self):  # вывод программного буффера дисплея на физическое устройство
+        self.Command(_SSD1306_COLUMNADDR)   # задаем нумерацию столбцов
+        self.Command(0)                     # Начало столбцов (0 = сброс)
+        self.Command(self.width-1)          # адрес последнего столбца
+        self.Command(_SSD1306_PAGEADDR)     # задаем адрес страниц (строк)
+        self.Command(0)                     # Начало строк (0 = сброс)
+        self.Command(self._pages-1)         # адрес последней строки
+        # Выводим буффер данных
+        for i in range(0, len(self._buffer), 16):
+            control = 0x40
+            self._WriteList(control, self._buffer[i:i+16])
+
+    def Image(self, image):     # выводит картинку созданную при помощи библиотеки PIL
+        # картинка должна быть в режиме mode = 1 и совпадать по размеру с дисплеем
+        if image.mode != '1':
+            raise ValueError('Image must be in mode 1.')
+        imWidth, imHeight = image.size
+        if imWidth != self.width or imHeight != self.height:
+            raise ValueError('Image must be same dimensions as display ({0}x{1})'.format(self.width, self.height))
+        pix = image.load()  # выгружаем пиксели из картинки
+        # проходим через память чтобы записать картинку в буффер
+        index = 0
+        for page in range(self._pages):
+            # идем по оси x (колонны)
+            for x in range(self.width):
+                bits = 0
+                for bit in [0, 1, 2, 3, 4, 5, 6, 7]:    # быстрее чем range
+                    bits = bits << 1
+                    bits |= 0 if pix[(x, page*8 + 7 - bit)] == 0 else 1
+                # обновляем буффер и увеличиваем счетчик
+                self._buffer[index] = bits
+                index += 1
+
+    def Clear(self):    # очищает буффер изображения
+        self._buffer = [0]*(self.width*self._pages)
+
+    def SetContrast(self, contrast):    # установка контрастности дисплея от 0 до 255
+        if contrast < 0 or contrast > 255:
+            raise  ValueError('Contrast must be value from 0 to 255 (inclusive).')
+        self.Command(_SSD1306_SETCONTRAST)
+        self.Command(contrast)
+
+    def Dim(self, dim):     # корректировка контрастности дисплея (пока не очень понял как оно работает!)
+        contrast = 0
+        if not dim:
+            if self._vccstate == _SSD1306_EXTERNALVCC:
+                contrast = 0x9F
+            else:
+                contrast = 0xCF
+
+    def _Write8(self, register, value):  # запись 8битного значения в заданный регистр
+        value = value & 0xFF
+        self._i2c.write_byte_data(_SSD1306_I2C_ADDRESS, register, value)
+
+    def _WriteList(self, register, data):
+        self._i2c.write_block_data(_SSD1306_I2C_ADDRESS, register, data)
+
+
+    def _ReadU8(self, register):    # чтение unsigned byte
+        result = self._i2c.read_byte_data(_SSD1306_I2C_ADDRESS, register) & 0xFF
+
+
+class SSD1306_128_64(SSD1306Base):  # класс для дисплея 128*64 pix
+    def __init__(self, i2cBus = None, i2cAddr = _SSD1306_I2C_ADDRESS):
+        # вызываем конструктор класса
+        super(SSD1306_128_64, self).__init__(128, 64, i2cBus, i2cAddr)
+
+    def _Initialize(self):
+        # инициализация конкретно для размера 128x64
+        self.Command(_SSD1306_DISPLAYOFF)           # 0xAE
+        self.Command(_SSD1306_SETDISPLAYCLOCKDIV)   # 0xD5
+        self.Command(0x80)                          # предлагаемоое соотношение 0x80
+        self.Command(_SSD1306_SETMULTIPLEX)         # 0xA8
+        self.Command(0x3F)
+        self.Command(_SSD1306_SETDISPLAYOFFSET)     # 0xD3
+        self.Command(0x0)                           # без отступов
+        self.Command(_SSD1306_SETSTARTLINE | 0x0)   # начинаем строки с 0
+        self.Command(_SSD1306_CHARGEPUMP)           # 0x8D
+        if self._vccstate == _SSD1306_EXTERNALVCC:
+            self.Command(0x10)
+        else:
+            self.Command(0x14)
+        self.Command(_SSD1306_MEMORYMODE)           # 0x20
+        self.Command(0x00)                          # иначе работает неправильно (0x0 act like ks0108)
+        self.Command(_SSD1306_SEGREMAP | 0x1)
+        self.Command(_SSD1306_COMSCANDEC)
+        self.Command(_SSD1306_SETCOMPINS)           # 0xDA
+        self.Command(0x12)
+        self.Command(_SSD1306_SETCONTRAST)          # 0x81
+        if self._vccstate == _SSD1306_EXTERNALVCC:
+            self.Command(0x9F)
+        else:
+            self.Command(0xCF)
+        self.Command(_SSD1306_SETPRECHARGE)         # 0xd9
+        if self._vccstate == _SSD1306_EXTERNALVCC:
+            self.Command(0x22)
+        else:
+            self.Command(0xF1)
+        self.Command(_SSD1306_SETVCOMDETECT)        # 0xDB
+        self.Command(0x40)
+        self.Command(_SSD1306_DISPLAYALLON_RESUME)  # 0xA4
+        self.Command(_SSD1306_NORMALDISPLAY)        # 0xA6
+
+
+class SSD1306_128_32(SSD1306Base):  # класс для дисплея 128*32 pix
+    def __init__(self, i2cBus = None, i2cAddr = _SSD1306_I2C_ADDRESS):
+        # Вызываем конструктор класса
+        super(SSD1306_128_32, self).__init__(128, 32, i2cBus, i2cAddr)
+
+    def _Initialize(self):
+        # Инициализация конкретно для размера 128x32 pix
+        self.Command(_SSD1306_DISPLAYOFF)           # 0xAE
+        self.Command(_SSD1306_SETDISPLAYCLOCKDIV)   # 0xD5
+        self.Command(0x80)                          # предлагаемоое соотношение 0x80
+        self.Command(_SSD1306_SETMULTIPLEX)         # 0xA8
+        self.Command(0x1F)
+        self.Command(_SSD1306_SETDISPLAYOFFSET)     # 0xD3
+        self.Command(0x0)                           # без отступов
+        self.Command(_SSD1306_SETSTARTLINE | 0x0)   # начинаем строки с 0
+        self.Command(_SSD1306_CHARGEPUMP)           # 0x8D
+        if self._vccstate == _SSD1306_EXTERNALVCC:
+            self.Command(0x10)
+        else:
+            self.Command(0x14)
+        self.Command(_SSD1306_MEMORYMODE)           # 0x20
+        self.Command(0x00)                          # иначе работает неправильно (0x0 act like ks0108)
+        self.Command(_SSD1306_SEGREMAP | 0x1)
+        self.Command(_SSD1306_COMSCANDEC)
+        self.Command(_SSD1306_SETCOMPINS)           # 0xDA
+        self.Command(0x02)
+        self.Command(_SSD1306_SETCONTRAST)          # 0x81
+        self.Command(0x8F)
+        self.Command(_SSD1306_SETPRECHARGE)         # 0xd9
+        if self._vccstate == _SSD1306_EXTERNALVCC:
+            self.Command(0x22)
+        else:
+            self.Command(0xF1)
+        self.Command(_SSD1306_SETVCOMDETECT)        # 0xDB
+        self.Command(0x40)
+        self.Command(_SSD1306_DISPLAYALLON_RESUME)  # 0xA4
+        self.Command(_SSD1306_NORMALDISPLAY)        # 0xA6
+
+
+class SSD1306_96_16(SSD1306Base):
+    def __init__(self, i2cBus = None, i2cAddr = _SSD1306_I2C_ADDRESS):
+        # Вызываем конструктор класса
+        super(SSD1306_96_16, self).__init__(96, 16, i2cBus, i2cAddr)
+
+    def _Initialize(self):
+        # Инициализация конкретно для размера 96x16 pix
+        self.Command(_SSD1306_DISPLAYOFF)           # 0xAE
+        self.Command(_SSD1306_SETDISPLAYCLOCKDIV)   # 0xD5
+        self.Command(0x60)                          # предлагаемоое соотношение 0x60
+        self.Command(_SSD1306_SETMULTIPLEX)         # 0xA8
+        self.Command(0x0F)
+        self.Command(_SSD1306_SETDISPLAYOFFSET)     # 0xD3
+        self.Command(0x0)                           # без отступов
+        self.Command(_SSD1306_SETSTARTLINE | 0x0)   # начинаем строки с 0
+        self.Command(_SSD1306_CHARGEPUMP)           # 0x8D
+        if self._vccstate == _SSD1306_EXTERNALVCC:
+            self.Command(0x10)
+        else:
+            self.Command(0x14)
+        self.Command(_SSD1306_MEMORYMODE)           # 0x20
+        self.Command(0x00)                          # иначе работает неправильно (0x0 act like ks0108)
+        self.Command(_SSD1306_SEGREMAP | 0x1)
+        self.Command(_SSD1306_COMSCANDEC)
+        self.Command(_SSD1306_SETCOMPINS)           # 0xDA
+        self.Command(0x02)
+        self.Command(_SSD1306_SETCONTRAST)          # 0x81
+        self.Command(0x8F)
+        self.Command(_SSD1306_SETPRECHARGE)         # 0xd9
+        if self._vccstate == _SSD1306_EXTERNALVCC:
+            self.Command(0x22)
+        else:
+            self.Command(0xF1)
+        self.Command(_SSD1306_SETVCOMDETECT)        # 0xDB
+        self.Command(0x40)
+        self.Command(_SSD1306_DISPLAYALLON_RESUME)  # 0xA4
+        self.Command(_SSD1306_NORMALDISPLAY)        # 0xA6
 
 
 ###
--- example.py
+++ example.py
@@ -2,44 +2,92 @@
 import RPiPWM
 import threading
 import time
-
+import subprocess
+from PIL import Image       # библиотеки для рисования на дисплее
+from PIL import ImageDraw
+from PIL import ImageFont
 
 exit = False
 
-pwm = RPiPWM.Pwm()
-pwm.InitChannel(0, RPiPWM.PwmMode.servo90)
+disp = RPiPWM.SSD1306_128_64()
+disp.Begin()
+disp.Clear()
+disp.Display()
 
-def ServoMover():
-    while not exit:
-        for i in range(90):
-            pwm.SetChannel(0, i)
-            time.sleep(0.01)
-        for i in range(90):
-            pwm.SetChannel(0, 90-i)
-            time.sleep(0.01)
+width = disp.width  # берем ширину и высоту из дисплея чтобы все точно совпадало
+height = disp.height
+image = Image.new('1', (width, height))
+draw = ImageDraw.Draw(image)    # создаем объект с помощью которого будем рисовать изображение для дисплея
 
+draw.rectangle((0, 0, width, height), outline=0, fill=0)    # прямоугольник, залитый черным чтобы очистить изображение
+# несколько переменных для удобства рисования
+padding = -2    # отступы
+top = padding
+bottom = height-padding
+# двигаемся слева на право, отслеживаем текущую позицию по x
+x = 0
+font = ImageFont.load_default()  # загружаем стандартный шрифт
+# можно подгрузить шрифт в формате ttf, если он лежит в той же папке
+# font = ImageFont.truetype('BlaBla.ttf', 8)
+while True:
+    # очищаем изображение
+    draw.rectangle((0, 0, width, height), outline=0, fill=0)
+    # Shell скрипты чтобы получить информацию о системе. Потом будем использовать библиотеку Psuils
+    cmd = "hostname -I | cut -d\' \' -f1"
+    IP = subprocess.check_output(cmd, shell = True )
+    cmd = "top -bn1 | grep load | awk '{printf \"CPU Load: %.2f\", $(NF-2)}'"
+    CPU = subprocess.check_output(cmd, shell = True )
+    cmd = "free -m | awk 'NR==2{printf \"Mem: %s/%sMB %.2f%%\", $3,$2,$3*100/$2 }'"
+    MemUsage = subprocess.check_output(cmd, shell = True )
+    cmd = "df -h | awk '$NF==\"/\"{printf \"Disk: %d/%dGB %s\", $3,$2,$5}'"
+    Disk = subprocess.check_output(cmd, shell = True )
 
-adc = RPiPWM.Battery(vRef=3.28, gain=7.66)
-print("Starting ADC")
-adc.start()
-value = 8.65
-print("Calibrating ADC with value %d" % value)
+    # Выводим строки текста
+    draw.text((x, top), "IP: " + str(IP), font=font, fill=255)
+    draw.text((x, top+8), str(CPU), font=font, fill=255)
+    draw.text((x, top+16), str(MemUsage), font=font, fill=255)
+    draw.text((x, top+25), str(Disk), font=font, fill=255)
+
+    # Выводим изображение
+    disp.Image(image)
+    disp.Display()
+    time.sleep(1)
+# TODO: написать пример, который будет работать со всеми элементами библиотеки сразу
+
+# pwm = RPiPWM.Pwm()
+# pwm.InitChannel(0, RPiPWM.PwmMode.servo90)
+#
+# def ServoMover():
+#     while not exit:
+#         for i in range(90):
+#             pwm.SetChannel(0, i)
+#             time.sleep(0.01)
+#         for i in range(90):
+#             pwm.SetChannel(0, 90-i)
+#             time.sleep(0.01)
+#
+#
+# adc = RPiPWM.Battery(vRef=3.28, gain=7.66)
+# print("Starting ADC")
+# adc.start()
+# value = 8.65
+# print("Calibrating ADC with value %d" % value)
 # adc.Calibrate(value)
-print("ADC started")
-
-gpio = RPiPWM.Gpio()
-
-
-def Informator():
-    while not exit:
-        print("Voltage: %.2f" % adc.GetVoltageFiltered())
-        time.sleep(1)
-    # gpio.LedToggle()
-
-gpio.ButtonAddEvent(Informator)
-
-t1 = threading.Thread(target=Informator)
-t1.start()
-t1.join()
-time.sleep(5)
-exit = True
+# print("ADC started")
+#
+# gpio = RPiPWM.Gpio()
+#
+#
+# def Informator():
+#     while not exit:
+#         print("Voltage: %.2f" % adc.GetVoltageFiltered())
+#         time.sleep(1)
+#     gpio.LedToggle()
+#
+# gpio.ButtonAddEvent(Informator)
+#
+# t1 = threading.Thread(target=Informator)
+# t1.start()
+# t1.join()
+# time.sleep(5)
+# exit = True
